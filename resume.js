9cc44ea45d1afc390f04d985d291225f8c9aa344
SohamD34 <deshmukh.2@iitj.ac.in>
2023-08-30 19:59:51 +0530

    Update Resume page

--- resume.js
+++ resume.js
@@ -9,3 +9,7 @@ hoverButton.addEventListener('mouseout', () => {
     hiddenDiv.style.display = 'none';
 });
 
+$('.horizontal .progress-fill span').each(function(){
+    var percent = $(this).html();
+    $(this).parent().css('width', percent);
+  });
