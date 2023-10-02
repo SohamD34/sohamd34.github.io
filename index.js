e89685b6e37157be528a7a1e31ab2cb01adb1715
SohamD34 <deshmukh.2@iitj.ac.in>
2023-10-02 19:46:38 +0530

    Changes in clock element

--- index.js
+++ index.js
@@ -27,3 +27,23 @@ function myFunction() {
     setTimeout(restoretext, 500);
 }
 
+function updateClock() {
+    const now = new Date();
+    const hour = now.getHours() % 12;
+    const minute = now.getMinutes();
+    const second = now.getSeconds();
+
+    const hourHand = document.getElementById("hour");
+    const minuteHand = document.getElementById("minute");
+    const secondHand = document.getElementById("second");
+
+    const hourRotation = (hour * 30) + (minute * 0.5); // Each hour mark is 30 degrees, each minute mark is 0.5 degrees
+    const minuteRotation = (minute * 6) + (second * 0.1); // Each minute mark is 6 degrees, each second mark is 0.1 degrees
+    const secondRotation = second * 6; // Each second mark is 6 degrees
+
+    hourHand.style.transform = `rotate(${hourRotation}deg)`;
+    minuteHand.style.transform = `rotate(${minuteRotation}deg)`;
+    secondHand.style.transform = `rotate(${secondRotation}deg)`;
+}
+
+setInterval(updateClock, 1);
